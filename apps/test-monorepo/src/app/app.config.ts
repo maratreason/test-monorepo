6dea0ff212befc64732f1dbb7af2094549aeeb8f
maratreason <maratreason@mail.ru>
2024-07-10 08:21:57 +0300

    feat(users) add data-access lib and add +state

--- apps/test-monorepo/src/app/app.config.ts
+++ apps/test-monorepo/src/app/app.config.ts
@@ -1,13 +1,30 @@
-import {ApplicationConfig, provideZoneChangeDetection} from "@angular/core";
+import {
+  ApplicationConfig,
+  provideZoneChangeDetection,
+  isDevMode,
+} from "@angular/core";
 import {provideRouter} from "@angular/router";
 import {provideHttpClient} from "@angular/common/http";
-import {API_URL} from "@users/core/http";
+import {provideStore, provideState} from "@ngrx/store";
+import {provideEffects} from "@ngrx/effects";
+import {provideStoreDevtools} from "@ngrx/store-devtools";
 
+import {API_URL} from "@users/core/http";
 import {appRoutes} from "./app.routes";
 import {environment} from "../environments/environment.development";
+import {UsersEffects, UsersFacade} from "@users/data-access";
+import * as fromUsers from "../../../../libs/users/users/data-access/src/lib/+state/users.reducer";
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideEffects(UsersEffects),
+    provideState(fromUsers.USERS_FEATURE_KEY, fromUsers.usersReducer),
+    UsersFacade,
+    provideStoreDevtools({
+      logOnly: !isDevMode(),
+      maxAge: 25,
+    }),
+    provideStore(),
     provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(appRoutes),
     provideHttpClient(),
@@ -18,4 +35,8 @@ export const appConfig: ApplicationConfig = {
   ],
 };
 
-// Остановился на уроке 10.
+// Остановился на уроке 12. 20:00.
+// Нужно социальное одобрение. Например:
+// - Марат, как ты во всём этом разбираешься? Ты пишешь код. Это так круто!
+// Для изучения NGRX нужно создать тестовый проект, прямо в либе users. И там использовать все фишки NGRX.
+// Entity, ngrx-store, ngrx-data, ngrx-component, ngrx-signals, ngrx-operators. Создать отдельный data-access и использовать его.
---
+++ libs/users/users/data-access/src/index.ts
@@ -0,0 +1,6 @@
+export * from "./lib/+state/users.actions";
+export * from "./lib/+state/users.effects";
+export * from "./lib/+state/users.facade";
+export * from "./lib/+state/users.models";
+export * from "./lib/+state/users.reducer";
+export * from "./lib/+state/users.selectors";
