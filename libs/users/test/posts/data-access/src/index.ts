0d5e4d583949baa32e79c73762b8036690a2569f
maratreason <maratreason@mail.ru>
2024-07-15 07:56:15 +0300

    refactor(posts) add imports on index.ts file

---
+++ libs/users/test/posts/data-access/src/index.ts
@@ -0,0 +1,5 @@
+export * from "./lib/+state/posts.actions";
+export * from "./lib/+state/posts.effects";
+export * from "./lib/+state/posts.facade";
+export * from "./lib/+state/posts.reducer";
+export * from "./lib/+state/posts.selectors";
