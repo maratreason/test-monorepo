16bbdcf5e2ee15dcb1154749b5e51eb9670c500e
maratreason <maratreason@mail.ru>
2024-07-15 07:25:54 +0300

    refactor(header) add router links

--- apps/test-monorepo/src/app/app.routes.ts
+++ apps/test-monorepo/src/app/app.routes.ts
@@ -4,10 +4,19 @@ export const appRoutes: Route[] = [
   {
     path: "",
     redirectTo: "home",
-    pathMatch: "full"
+    pathMatch: "full",
   },
   {
     path: "home",
-    loadComponent: () => import("@users/home").then(c => c.HomeComponent)
-  }
+    loadComponent: () => import("@users/home").then((c) => c.HomeComponent),
+  },
+  {
+    path: "posts",
+    loadComponent: () =>
+      import("@users/test/posts").then((c) => c.PostsListComponent),
+  },
+  {
+    path: "contacts",
+    redirectTo: "home",
+  },
 ];
--- libs/users/core/ui/layout/src/lib/header/header.component.ts
+++ libs/users/core/ui/layout/src/lib/header/header.component.ts
@@ -3,11 +3,12 @@ import {CommonModule} from "@angular/common";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: "ang-header",
   standalone: true,
-  imports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule, RouterLink],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
