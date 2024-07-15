ec69539208486a693b8ac38d65fa5816f5ec1185
maratreason <maratreason@mail.ru>
2024-07-15 06:58:29 +0300

    refactor(layout) add styles to header and footer

--- libs/users/core/ui/layout/src/lib/footer/footer.component.ts
+++ libs/users/core/ui/layout/src/lib/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import {ChangeDetectionStrategy, Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @Component({
   selector: "ang-footer",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatToolbarModule],
   templateUrl: "./footer.component.html",
   styleUrl: "./footer.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
--- libs/users/core/ui/layout/src/lib/header/header.component.ts
+++ libs/users/core/ui/layout/src/lib/header/header.component.ts
@@ -1,10 +1,13 @@
 import {ChangeDetectionStrategy, Component} from "@angular/core";
 import {CommonModule} from "@angular/common";
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 @Component({
   selector: "ang-header",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatIconModule, MatButtonModule, MatToolbarModule],
   templateUrl: "./header.component.html",
   styleUrl: "./header.component.scss",
   changeDetection: ChangeDetectionStrategy.OnPush,
