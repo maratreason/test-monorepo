c2d3525081741f23821b21cc9486d4c10b17ac4f
maratreason <maratreason@mail.ru>
2024-07-26 19:23:20 +0300

    feat(feature-users-detail) add users-detail-vm

---
+++ libs/users/users/feature-users-detail/src/lib/users-detail-vm.ts
@@ -0,0 +1,8 @@
+import {DeepReadonly} from "@users/core/utils";
+import {UsersStatus} from "@users/data-access";
+import {CreateUserDTO} from "libs/users/users/data-access/src/lib/users-dto.model";
+
+export type UsersDetailVM = DeepReadonly<{
+  user: CreateUserDTO;
+  status: UsersStatus;
+}>;
